72dcd2d2bab71d0e3cb0de4eb960c12d306f1b7b
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2012-09-18 12:43:44 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeoAuditor::Application.config.secret_token = 'e2931e79bcc058fc61bf4dd50cbb7d94b3cde686593826613404dfc5aa85351395b50dd0d7cc2ed69715ca628a4a4f75c5a9c300abf7b7a543b72337da505617'
