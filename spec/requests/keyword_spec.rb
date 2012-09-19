fc90bb06a7fef289cbfdd364a21f5d351c20ddcf
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2012-09-19 17:06:08 +0100

    better navigation to keywords pages

--- spec/requests/keyword_spec.rb
+++ spec/requests/keyword_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe "Keywords pages" do
   let(:project) { Factory.create(:project) }
 
-  it "viewing the keyword index and deletiung one" do
+  it "viewing the keyword index" do
     2.times { Factory.create(:keyword, :project => project) }
 
-    visit project_keywords_path project
+    visit project_path project
+    click_link "View keywords"
+    current_path.should eql project_keywords_path project
 
     within "table" do
       page.should have_selector "tr", :count => 2
@@ -14,12 +16,14 @@ describe "Keywords pages" do
   end
 
   it "creating keywords" do
-    visit new_project_keyword_path project
+    visit project_keywords_path project
+    click_link "Create new keyword"
+    current_path.should eql new_project_keyword_path project
 
     fill_in "Name", :with => "agile"
     click_button "Add Keyword"
+    current_path.should eql project_keywords_path project
 
-    current_path.should eql project_keywords_path(project)
     # TODO - flash messages
     # page.should have_content "Keyword successfully created."
     within "table" do
